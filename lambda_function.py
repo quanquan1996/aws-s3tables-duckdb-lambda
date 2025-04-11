e09e24fb9ad94bda6c05a07d4a7e05307835f2fd
pengjunq <1467069932@qq.com>
2025-04-11 13:28:56 +0800

    init

---
+++ lambda_function.py
@@ -0,0 +1,39 @@
+import duckdb
+import boto3
+
+con = duckdb.connect(database=':memory:', config={'memory_limit': '9GB','worker_threads': 5})
+con.execute("""
+FORCE INSTALL aws FROM core_nightly;
+FORCE INSTALL httpfs FROM core_nightly;
+FORCE INSTALL iceberg FROM core_nightly;
+CREATE SECRET (
+    TYPE s3,
+    PROVIDER credential_chain
+);
+""")
+s3tables = boto3.client('s3tables')
+table_buckets = s3tables.list_table_buckets(maxBuckets=1000)['tableBuckets']
+for table_bucket in table_buckets:
+    name = table_bucket['name']
+    arn = table_bucket['arn']
+    con.execute(
+        f"""
+        ATTACH '{arn}' AS {name} (
+        TYPE iceberg,
+        ENDPOINT_TYPE s3_tables
+    );
+        """
+    )
+def handler(event, context):
+    sql = event.get("sql")
+    try:
+        result = con.execute(sql).fetchall()
+        return {
+            "statusCode": 200,
+            "result": result
+        }
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "error": str(e)
+        }
